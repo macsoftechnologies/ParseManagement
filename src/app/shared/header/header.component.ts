65a9588473b723445cebec3c48360d8c2a740c18
macsoftechnologies <macsoftechnologies@gmail.com>
2021-11-20 11:56:28 +0530

    code comitted

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+declare let $: any;
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  classList: any;
   constructor() {}
 
   ngOnInit(): void {}
+  showMenu() {
+    $('.menu').classList.toggle('open-menu');
+    $('.open-icon').classList.toggle('close-icon');
+  }
 }
